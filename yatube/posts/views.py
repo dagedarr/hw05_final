33a4a5ad0ba99d356e39f46800133082f97d7fc2
Natolin Artem <arnat64@yandex.ru>
2022-12-16 01:27:01 +0300

    first_try

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -13,6 +13,7 @@ def pagination(request, post_list):
     page_number = request.GET.get('page')
     return paginator.get_page(page_number)
 
+
 @cache_page(settings.CACHE_TIME, key_prefix='index_page')
 def index(request):
     context = {
@@ -64,7 +65,7 @@ def post_create(request):
         request.POST or None,
         files=request.FILES or None
     )
-    
+
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
@@ -76,7 +77,7 @@ def post_create(request):
 @login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    
+
     form = PostForm(
         request.POST or None,
         instance=post,
@@ -85,13 +86,14 @@ def post_edit(request, post_id):
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail', post_id)
-        
+
     context = {
         'form': form,
         'post': post,
         'is_edit': True,
     }
     return render(request, 'posts/create_post.html', context)
+
 
 @login_required
 def add_comment(request, post_id):
@@ -102,7 +104,8 @@ def add_comment(request, post_id):
         comment.author = request.user
         comment.post = post
         comment.save()
-    return redirect('posts:post_detail', post_id=post_id) 
+    return redirect('posts:post_detail', post_id=post_id)
+
 
 @login_required
 def follow_index(request):
@@ -113,6 +116,7 @@ def follow_index(request):
     }
     return render(request, 'posts/follow.html', context)
 
+
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
@@ -120,6 +124,7 @@ def profile_follow(request, username):
         return redirect('posts:profile', author)
     Follow.objects.get_or_create(user=request.user, author=author)
     return redirect('posts:profile', author)
+
 
 @login_required
 def profile_unfollow(request, username):
